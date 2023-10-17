0c6cb5125f6cc7e13187b377a1a316b608fcc2a9
Felipe Nogueira <felipe.nogueira@maeztra.com>
2023-10-17 19:45:58 -0300

    criando estilos com StyleSheet

--- App.tsx
+++ App.tsx
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet } from "react-native"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <>
+      <View style={styles.container}>
+        <Text style={styles.eventName}>Nome do evento</Text>
+        <Text style={styles.eventDate}>Felipe Nogueira</Text>
+      </View>
+    </>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#131016',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 24,
   },
-});
+  eventName: {
+    color: '#fdfcfe',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48,
+  },
+  eventDate: {
+    color: '#6b6b6b',
+    fontSize: 16,
+  },
+})
